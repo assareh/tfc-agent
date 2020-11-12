f7ee7f20d0f13e6d5b3a58c116b20ca475e81127
assareh <assareh@hashicorp.com>
2020-11-12 10:01:46 -0800

    added argument to specify agent pool

--- tfc-agent-ecs/producer/files/change_ws_exec_mode.sh
+++ tfc-agent-ecs/producer/files/change_ws_exec_mode.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # NOTE: ensure TFC token is present as TOKEN env variable
-# usage: ./change_ws_exec_mode.sh <YOUR TFC ORG> <YOUR TFC WORKSPACE> [Optionally additional workspaces]
+# usage: ./change_ws_exec_mode.sh <YOUR TFC ORG> <YOUR AGENT POOL NAME> <YOUR TFC WORKSPACE> [Optionally additional workspaces]
 TFC_ORG=$1
+AGENT_POOL_NAME=$2
 
 if [ -z "$TOKEN" ]
 then
       echo "Missing required environment variable TOKEN, see usage."
 else
 
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
   then
     echo "Missing required arguments, see usage."
   else
@@ -19,7 +20,7 @@ AGENT_POOL_ID=$(curl --silent \
   --header "Authorization: Bearer $TOKEN" \
   --header "Content-Type: application/vnd.api+json" \
   --request GET \
-  https://app.terraform.io/api/v2/organizations/$TFC_ORG/agent-pools | jq -r '.data[0].id')
+  https://app.terraform.io/api/v2/organizations/$TFC_ORG/agent-pools | jq -r --arg AGENT_POOL_NAME "$AGENT_POOL_NAME" '.data[] | select(.attributes.name==$AGENT_POOL_NAME)' | jq -r '.id')
 
 # skip first argument since already captured, and loop over each workspace
 shift
