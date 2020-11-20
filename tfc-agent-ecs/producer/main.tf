3cecb6b3569142fc44a316fb8ef2ab3513368925
Andy Assareh <assareh@me.com>
2020-11-19 21:39:14 -0800

    formatting

--- tfc-agent-ecs/producer/main.tf
+++ tfc-agent-ecs/producer/main.tf
@@ -205,8 +205,7 @@ resource "aws_security_group_rule" "allow_egress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks = [
-  "0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # lambda
