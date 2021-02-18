41c88ca4604ae4f6ce7e88eee035781f7123746b
Andy Assareh <assareh@hashicorp.com>
2021-02-18 09:41:33 -0800

    added output - public dns

--- tfc-agent-ecs/consumer/outputs.tf
+++ tfc-agent-ecs/consumer/outputs.tf
@@ -1,8 +1,11 @@
 output "caller" {
-  value       = data.aws_caller_identity.current.arn
-  sensitive   = true
+  value = data.aws_caller_identity.current.arn
 }
 
-output "instance_id" {
-  value = aws_instance.vault.id
+output "instance_arn" {
+  value = aws_instance.vault.arn
+}
+
+output "public_dns" {
+  value = aws_instance.vault.public_dns
 }
