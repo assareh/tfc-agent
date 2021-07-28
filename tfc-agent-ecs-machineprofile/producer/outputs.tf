fa70cd3ce694e6f8e7ce58d201eb9b9b3e8be0be
ppresto <ppresto@hashicorp.com>
2021-07-27 17:24:06 -0700

    test1

--- tfc-agent-ecs-machineprofile/producer/outputs.tf
+++ tfc-agent-ecs-machineprofile/producer/outputs.tf
@@ -1,11 +1,3 @@
-output "terraform_dev_role" {
-  value = aws_iam_role.terraform_dev_role.arn
-}
-
-output "iam_role_ecs_agent" {
-  value = aws_iam_role.agent.arn
-}
-
 output "webhook_url" {
   value = aws_api_gateway_deployment.webhook.invoke_url
 }
---
+++ tfc-agent-ecs-machineprofile/producer/outputs.tf.orig
@@ -0,0 +1,11 @@
+output "terraform_dev_role" {
+  value = aws_iam_role.terraform_dev_role.arn
+}
+
+output "iam_role_ecs_agent" {
+  value = aws_iam_role.agent.arn
+}
+
+output "webhook_url" {
+  value = aws_api_gateway_deployment.webhook.invoke_url
+}
