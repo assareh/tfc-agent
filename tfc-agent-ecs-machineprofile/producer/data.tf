f34a3c4397b692236b3a8833620058a1fd615f9e
ppresto <ppresto@hashicorp.com>
2021-07-28 16:14:19 -0700

    add remote state variables

---
+++ tfc-agent-ecs-machineprofile/producer/data.tf
@@ -0,0 +1,9 @@
+// Workspace Data
+data "terraform_remote_state" "presto_projects_ws_aws_iam" {
+  backend = "atlas"
+  config = {
+    address = "https://app.terraform.io"
+    name    = "presto-projects/ws_aws_agent_iam"
+  }
+}
+# data.terraform_remote_state.presto_projects_ws_aws_iam.outputs.name
