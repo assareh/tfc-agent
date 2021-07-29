13393606beea7106df805f4a8dc67f1a45cb3e03
ppresto <ppresto@hashicorp.com>
2021-07-28 17:53:51 -0700

    remote state ws name update

--- tfc-agent-ecs-machineprofile/producer/data.tf
+++ tfc-agent-ecs-machineprofile/producer/data.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "presto_projects_ws_aws_iam" {
   backend = "atlas"
   config = {
     address = "https://app.terraform.io"
-    name    = "presto-projects/ws_aws_agent_iam"
+    name    = "presto-projects/ws_aws_iam"
   }
 }
 # data.terraform_remote_state.presto_projects_ws_aws_iam.outputs.name
