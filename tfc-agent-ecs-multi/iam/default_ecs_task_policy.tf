cfecb116671eb14b53839b226b62e9876e695c21
ppresto <ppresto@hashicorp.com>
2021-07-30 17:36:15 -0700

    clean up file

--- tfc-agent-ecs-multi/iam/default_ecs_task_policy.tf
+++ tfc-agent-ecs-multi/iam/default_ecs_task_policy.tf
@@ -14,19 +14,3 @@ data "aws_iam_policy_document" "ecs_assume_role_policy_definition" {
     }
   }
 }
-
-# ECS Task Policy that will assume a specific Service Role
-data "aws_iam_policy_document" "ecs_assume_role_policy_serviceA" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      identifiers = ["ecs-tasks.amazonaws.com"]
-      type        = "Service"
-    }
-    principals {
-      identifiers = [aws_iam_role.tfc_agent_task_A.arn]
-      type        = "AWS"
-    }
-  }
-}
