b9029165041b551a4762265d64f8683eb808e23f
ppresto <ppresto@hashicorp.com>
2021-08-25 13:11:30 -0700

    define zone

--- tfc-agent-gke/tfc-agent/variables.tf
+++ tfc-agent-gke/tfc-agent/variables.tf
@@ -2,3 +2,7 @@ variable "tfc_agent_token" {
   type        = string
   description = "The preconfigured Terraform Cloud Agent token"
 }
+variable "gcp_zone" {
+  type        = string
+  default = "us-west1-c"
+}
