89c9857a3352b87b18e404a59784d53616f00345
ppresto <ppresto@hashicorp.com>
2021-08-25 11:26:23 -0700

    empty creds

--- tfc-agent-gke/tfc-agent/main.tf
+++ tfc-agent-gke/tfc-agent/main.tf
@@ -12,6 +12,8 @@ provider "kubernetes" {
   load_config_file = false
   host = "https://${data.terraform_remote_state.gke.outputs.k8s_endpoint}"
   config_context = data.terraform_remote_state.gke.outputs.context
+  username = ""
+  password = ""
   client_certificate     = "${base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_certificate)}"
   client_key             = "${base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_key)}"
   cluster_ca_certificate = "${base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_cluster_ca_certificate)}"
