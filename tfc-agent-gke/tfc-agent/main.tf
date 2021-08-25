ce5581d0916525741434eaacf3ddd77685dcc672
ppresto <ppresto@hashicorp.com>
2021-08-25 13:07:53 -0700

    adding client token

--- tfc-agent-gke/tfc-agent/main.tf
+++ tfc-agent-gke/tfc-agent/main.tf
@@ -6,15 +6,21 @@ data "terraform_remote_state" "gke" {
     name    = "presto-projects/gke_cluster"
   }
 }
+data "google_client_config" "default" {}
+data "google_container_cluster" "my_cluster" {
+  name = data.terraform_remote_state.gke.outputs.kubernetes_cluster_name
+  zone = data.terraform_remote_state.gke.outputs.zone
+}
 
 provider "kubernetes" {
   #version = "~> 1.12"
   load_config_file = false
   host = "https://${data.terraform_remote_state.gke.outputs.k8s_endpoint}"
   config_context = data.terraform_remote_state.gke.outputs.context
-  #client_certificate     = "${base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_certificate)}"
-  #client_key             = "${base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_key)}"
-  #cluster_ca_certificate = "${base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_cluster_ca_certificate)}"
+  token                  = data.google_client_config.default.access_token
+  #client_certificate     = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_certificate)
+  #client_key             = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_key)
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_cluster_ca_certificate)
 }
 
 module "tfc_agent" {
