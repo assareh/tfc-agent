ee6ee7195f897f9763da5b263141c6b819ea3959
ppresto <ppresto@hashicorp.com>
2021-08-26 15:53:43 -0700

    add var.gcp_project

--- tfc-agent-gke/gke_tfcagents/variables.tf
+++ tfc-agent-gke/gke_tfcagents/variables.tf
@@ -1,8 +1,13 @@
-variable "tfc_agent_token" {
-  type        = string
-  description = "The preconfigured Terraform Cloud Agent token"
+variable "gcp_project" {
+  description = "GCP Project ID can be sourced from Env.  Prefix with TF_VAR_"
 }
+
 variable "gcp_zone" {
   type        = string
   default = "us-west1-c"
+}
+
+variable "tfc_agent_token" {
+  type        = string
+  description = "The preconfigured Terraform Cloud Agent token"
 }
