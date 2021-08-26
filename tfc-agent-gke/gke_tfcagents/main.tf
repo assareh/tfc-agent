adadf1297e56a36d71c38936272f878e322986f3
ppresto <ppresto@hashicorp.com>
2021-08-26 15:49:20 -0700

    3

--- tfc-agent-gke/gke_tfcagents/main.tf
+++ tfc-agent-gke/gke_tfcagents/main.tf
@@ -33,6 +33,6 @@ module "tfc_agent" {
   namespace_creation_enabled = true
   kubernetes_namespace       = "tfc-agent"
   service_account_annotations = {
-    iam.gke.io/gcp-service-account = workload-identity-tutorial@${var.project}.iam.gserviceaccount.com,
+    "iam.gke.io/gcp-service-account = workload-identity-tutorial@${var.project}.iam.gserviceaccount.com",
   }
 }
