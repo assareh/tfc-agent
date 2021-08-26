326d767e7eb32972625fefeefc452566ada9fd58
ppresto <ppresto@hashicorp.com>
2021-08-25 19:46:04 -0700

    remove userinfo-email scope

--- tfc-agent-gke/modules/gcp-vpc-gke/main.tf
+++ tfc-agent-gke/modules/gcp-vpc-gke/main.tf
@@ -38,8 +38,7 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/cloud-platform",
-      "https://www.googleapis.com/auth/userinfo-email"
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
 
     labels = {
