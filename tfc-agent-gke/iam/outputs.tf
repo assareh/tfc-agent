846194f71a81e07c2b3588ac0e1053af23220b3f
ppresto <ppresto@hashicorp.com>
2021-08-27 08:35:38 -0700

    remove instances

--- tfc-agent-gke/iam/outputs.tf
+++ tfc-agent-gke/iam/outputs.tf
@@ -2,10 +2,6 @@ output "gcp_project" {
   value = data.google_project.project.id
 }
 
-output "instance_names" {
-  value = google_compute_instance.tfc-agent.*.name
-}
-
 output "k8s_cluster_sa_email" {
   value = google_service_account.cluster-serviceaccount.email
 }
