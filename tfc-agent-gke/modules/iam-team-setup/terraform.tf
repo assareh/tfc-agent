c87b6f53196dace12e0a5f7a13d495156629bdb8
ppresto <ppresto@hashicorp.com>
2021-09-01 14:01:21 -0700

    upgrade tf 1.0.5

--- tfc-agent-gke/modules/iam-team-setup/terraform.tf
+++ tfc-agent-gke/modules/iam-team-setup/terraform.tf
@@ -1,15 +1,5 @@
-provider "google" {
-  project = var.gcp_project
-  region  = var.gcp_region
-}
-
-provider "tfe" {
-  hostname = var.tfe_hostname
-  token = var.tfe_token
-}
-
 terraform {
-  required_version = ">= 0.13.06"
+  required_version = ">= 1.0"
   required_providers {
     google = {
       version = "~> 3.81"
