6aba51e5da4ba9aa970bc7977d501c569602df96
ppresto <ppresto@hashicorp.com>
2021-09-02 16:04:14 -0700

    1

--- tfc-agent-gke/modules/workspace-mgr/outputs.tf
+++ tfc-agent-gke/modules/workspace-mgr/outputs.tf
@@ -2,5 +2,5 @@ output "Workspace" {
   value = var.workspacename
 }
 output "ws-id" {
-  value = var.identifier != "" ? tfe_workspace.ws-vcs[0].id : tfe_workspace.ws-novcs[0].id
+  value = tfe_workspace.ws-vcs.id
 }
