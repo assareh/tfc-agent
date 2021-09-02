6490bcdf0bdbd25948e3f0c3113fe450e1413832
ppresto <ppresto@hashicorp.com>
2021-09-01 17:01:10 -0700

    6

--- tfc-agent-gke/tfcb_workspaces/outputs.tf
+++ tfc-agent-gke/tfcb_workspaces/outputs.tf
@@ -8,7 +8,21 @@
 #}
 
 output "team_agentpool_ids" {
-    value = { for t in sort(keys(var.iam_teams)) : 
-        t => [module.iam-team-setup[t].agentpool_id, module.iam-team-setup[t].team]
+    value = { for t in sort(keys(var.iam_teams)) :
+        t => module.iam-team-setup[t].agentpool_id
+    }
+}
+
+output "teams" {
+    value = { 
+        flatten([for team, value in var.team:
+                  {
+                  "name" = value.name
+                  "roles" = value.roles
+                  "gsa" = value.gsa
+                  "k8s_sa" = value.k8s_sa
+                  "namespace" = value.namespace
+                  "agentpool_id" = module.iam-team-setup[var.team].agentpool_id}
+                ])
     }
 }
