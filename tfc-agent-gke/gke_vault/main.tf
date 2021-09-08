0c0616cc9ac935e13216762ca8c0de7c842d43a5
ppresto <ppresto@hashicorp.com>
2021-09-08 12:47:56 -0700

    install vault

---
+++ tfc-agent-gke/gke_vault/main.tf
@@ -0,0 +1,45 @@
+// Workspace Data
+data "terraform_remote_state" "gke" {
+  backend = "atlas"
+  config = {
+    address = "https://app.terraform.io"
+    name    = "presto-projects/gke"
+  }
+}
+
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "my_cluster" {
+  name = data.terraform_remote_state.gke.outputs.kubernetes_cluster_name
+  location = var.gcp_zone
+}
+
+provider "kubernetes" {
+  #version = "~> 1.12"
+  load_config_file = false
+  host = "https://${data.terraform_remote_state.gke.outputs.k8s_endpoint}"
+  config_context = data.terraform_remote_state.gke.outputs.context
+  token                  = data.google_client_config.default.access_token
+  #client_certificate     = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_certificate)
+  #client_key             = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_client_key)
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_cluster_ca_certificate)
+}
+
+resource "helm_release" "vault" {
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "hashicorp/vault"
+
+  #values = [
+  #  file("${path.module}/override-values.yaml")
+  #]
+  set {
+    name  = "server.dev.enabled"
+    value = "true"
+  }
+
+  set_sensitive {
+    name  = "tfc.token"
+    value = var.tfc_app_token
+  }
+}
---
+++ tfc-agent-gke/gke_vault/variables.tf
@@ -0,0 +1,15 @@
+variable "gcp_project" {
+  description = "GCP Project ID can be sourced from Env.  Prefix with TF_VAR_"
+}
+
+variable "gcp_zone" {
+  type        = string
+  default = "us-west1-c"
+}
+
+variable "tfc_app_token" {
+  type        = string
+  description = "Terraform Cloud Agent Token"
+  default = ""
+}
+
