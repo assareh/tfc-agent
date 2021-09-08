1d1e2d9ecee26ce3eae68d848dfe80cfb6eb6e6d
ppresto <ppresto@hashicorp.com>
2021-09-08 13:09:51 -0700

    k8s auth

--- tfc-agent-gke/gke_vault/main.tf
+++ tfc-agent-gke/gke_vault/main.tf
@@ -25,6 +25,16 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_cluster_ca_certificate)
 }
 
+provider "helm" {
+  kubernetes {
+    load_config_file = false
+    host                   = "https://${data.terraform_remote_state.gke.outputs.k8s_endpoint}"
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.gke.outputs.k8s_master_auth_cluster_ca_certificate)
+    config_context         = data.terraform_remote_state.gke.outputs.context
+    token                  = data.google_client_config.default.access_token
+  }
+}
+
 resource "helm_release" "vault" {
   name       = "hashicorp/vault"
   repository = "https://helm.releases.hashicorp.com"
