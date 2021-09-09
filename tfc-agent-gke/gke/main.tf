854859f1de0e437b961504ea4ad4ae12a66522b1
ppresto <ppresto@hashicorp.com>
2021-09-09 15:23:48 -0700

    vault injector

--- tfc-agent-gke/gke/main.tf
+++ tfc-agent-gke/gke/main.tf
@@ -20,3 +20,26 @@ module "gcp-vpc-gke" {
   gke_service_account_email = var.gke_service_account_email
   #gke_namespace  = var.gke_namespace
 }
+
+// Install Vault injector
+data "google_client_config" "default" {}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gcp-vpc-gke.k8s_endpoint}"
+    cluster_ca_certificate = base64decode(module.gcp-vpc-gke.k8s_master_auth_cluster_ca_certificate)
+    config_context         = module.gcp-vpc-gke.context
+    token                  = data.google_client_config.default.access_token
+  }
+}
+
+resource "helm_release" "vault" {
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  version    = "0.15.0"
+  set {
+    name  = "injector.externalVaultAddr"
+    value = "http://external-vault:8200"
+  }
+}
--- tfc-agent-gke/gke_vault/main.tf
+++ tfc-agent-gke/gke_vault/main.tf
@@ -65,5 +65,5 @@ data "kubernetes_service" "vault_ingress" {
 }
 
 output "ip" {
-  value = data.kubernetes_service.vault_ingress
+  value = data.kubernetes_service.vault_ingress.status.0.load_balancer.0.ingress.0.ip
 }
