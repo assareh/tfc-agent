a369b3380d2db952c1dd0162998029ac2b39ee92
ppresto <ppresto@hashicorp.com>
2021-09-09 15:54:21 -0700

    2

--- tfc-agent-gke/gke/main.tf
+++ tfc-agent-gke/gke/main.tf
@@ -21,6 +21,16 @@ provider "helm" {
   }
 }
 
+// Kubernetes resources
+provider "kubernetes" {
+  #version = "~> 1.12"
+  load_config_file = false
+  host                   = "https://${module.gcp-vpc-gke.k8s_endpoint}"
+  cluster_ca_certificate = base64decode(module.gcp-vpc-gke.k8s_master_auth_cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+  #config_context = data.terraform_remote_state.gke.outputs.context
+}
+
 resource "helm_release" "vault" {
   name       = "vault"
   repository = "https://helm.releases.hashicorp.com"
