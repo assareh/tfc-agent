34fc1d09632b871e5d3f931f9acc09beecff2b22
ppresto <ppresto@hashicorp.com>
2021-09-15 12:08:30 -0700

    fix outputs

--- tfc-agent-gke/gke_tfcagents/outputs.tf
+++ tfc-agent-gke/gke_tfcagents/outputs.tf
@@ -1,6 +1,6 @@
 output "gke_sa" {
-  value = module.tfc_agent2.gke_sa
+  value = module.tfc_agent.gke_sa
 }
 output "gke_deployment" {
-  value = module.tfc_agent2.gke_deployment
+  value = module.tfc_agent.gke_deployment
 }
