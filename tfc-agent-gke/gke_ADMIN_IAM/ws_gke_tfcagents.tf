f7ec644dccd588fc2fff7f3dabcc2fb3bcd0ec6a
ppresto <ppresto@hashicorp.com>
2021-09-16 11:25:30 -0700

    2

--- tfc-agent-gke/gke_ADMIN_IAM/ws_gke_tfcagents.tf
+++ tfc-agent-gke/gke_ADMIN_IAM/ws_gke_tfcagents.tf
@@ -21,14 +21,16 @@ module "gke_svc_tfcagents" {
     "GOOGLE_CREDENTIALS" : var.gcp_credentials
     }
     tf_variables = {
-        "prefix" = "presto"
-        "gcp_project" = var.gcp_project
-        "gcp_region" = "us-west1"
-        "gcp_zone" = "us-west1-c"
-        "namespace" = "tfc-team1"
-        "environment" = "dev"
-        "agentpool_tokens" = jsonencode(for t in sort(keys(var.iam_teams)) :
-        t => {"agent_token" : module.iam-team-setup[t].agent_token})
+        "prefix" = "presto",
+        "gcp_project" = var.gcp_project,
+        "gcp_region" = "us-west1",
+        "gcp_zone" = "us-west1-c",
+        "namespace" = "tfc-team1",
+        "environment" = "dev",
+        "agentpool_tokens" = jsonencode(
+            for t in sort(keys(var.iam_teams)) :
+            t => {"agent_token" : module.iam-team-setup[t].agent_token},
+        )
     }
     tf_variables_sec = {
         for t in sort(keys(var.iam_teams)) :
