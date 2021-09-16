56be5c5d4e23350b5965e4da3cb6038b922bea89
ppresto <ppresto@hashicorp.com>
2021-09-16 13:13:44 -0700

    2

--- tfc-agent-gke/modules/iam-team-setup/outputs.tf
+++ tfc-agent-gke/modules/iam-team-setup/outputs.tf
@@ -6,9 +6,9 @@ output "agentpool_id" {
   value = tfe_agent_pool.team_pool.id
 }
 
-#output "agent_token" {
-#  value = tfe_agent_token.team_agent_token.token
-#}
+output "agent_token" {
+  value = tfe_agent_token.team_agent_token.token
+}
 
 output "team" {
   value = var.team
