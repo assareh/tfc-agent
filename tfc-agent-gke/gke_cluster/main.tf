39b441c0c9b15ba84451ee41b811e9fb8dab163a
ppresto <ppresto@hashicorp.com>
2021-09-24 14:02:51 -0700

    2

--- tfc-agent-gke/gke_cluster/main.tf
+++ tfc-agent-gke/gke_cluster/main.tf
@@ -42,7 +42,7 @@ resource "kubernetes_namespace" "namespace" {
   for_each = local.teams
   metadata {
     name = local.teams[each.key].namespace
-    labels {
+    labels = {
       gke_ver = module.gcp-vpc-gke.k8s_master_version
     }
   }
