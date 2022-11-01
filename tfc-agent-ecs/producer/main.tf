e2cdd7595c18ff3c05a9cbd81dab2131c0a83f07
Andy Assareh <assareh@me.com>
2022-10-31 17:02:06 -0700

    making bucket private

--- tfc-agent-ecs/producer/main.tf
+++ tfc-agent-ecs/producer/main.tf
@@ -271,6 +271,15 @@ resource "aws_s3_bucket_acl" "webhook" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "webhook" {
+  bucket = aws_s3_bucket.webhook.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_object" "webhook" {
   bucket = aws_s3_bucket.webhook.id
   key    = "v${var.app_version}/webhook.zip"
